d91d293100c47e3e16be23dfd4be3f324e776e74
MD <dzhabrailov.magomed3@gmail.com>
2021-10-28 10:52:18 +0300

    linked list an isertion of node at a beginning  version1

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,12 @@
 package main
 
-import "github.com/Muhammad-D/data_structures_golang_implementation/linked_list/implementation"
+import (
+	"github.com/Muhammad-D/data_structures_golang_implementation/linked_list/insert_at_beginning"
+)
 
 func main() {
 
-	implementation.Start()
+	// implementation.Start()
+	insert_at_beginning.Start()
 
 }
---
+++ linked_list/insert_at_beginning/insert_at_beginning.go
@@ -0,0 +1,40 @@
+package insert_at_beginning
+
+import (
+	"fmt"
+
+	"github.com/Muhammad-D/data_structures_golang_implementation/linked_list/model"
+)
+
+func Start() {
+
+	var head *model.Node
+
+	var n, x int
+
+	fmt.Print("How many nodes there should be? ")
+	fmt.Scanln(&n)
+
+	for i := 0; i < n; i++ {
+		fmt.Print("Enter value ")
+		fmt.Scanln(&x)
+		insert(x, &head)
+		print(head)
+	}
+
+}
+
+func insert(n int, hp **model.Node) {
+	temp := model.New(n)
+	temp.Link = *hp
+	*hp = temp
+}
+
+func print(hp *model.Node) {
+	fmt.Printf("List consists of nex values: ")
+	for hp != nil {
+		fmt.Printf("%d, ", hp.Num)
+		hp = hp.Link
+	}
+	fmt.Println()
+}
