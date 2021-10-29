41106c5013900d63b72eb7fd3c5524a7c379f863
Muhammad Dzhabrailov <Muhammad@Muhammads-MacBook-Pro.local>
2021-10-29 08:46:54 +0300

    insert a node at nth position version1

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,10 @@
 package main
 
-import (
-	"github.com/Muhammad-D/data_structures_golang_implementation/linked_list/insert_at_beginning"
-)
+import "github.com/Muhammad-D/data_structures_golang_implementation/linked_list/insert_at_nth"
 
 func main() {
 
 	// implementation.Start()
-	insert_at_beginning.Start()
+	insert_at_nth.Start()
 
 }
